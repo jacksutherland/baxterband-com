9401aae242551cc440f69b241c070133d9ba0d30
Jack Sutherland <jacksutherl@gmail.com>
2018-08-03 10:54:33 -0400

    smoke

---
+++ src/js/site.js
@@ -0,0 +1,27 @@
+function randomNumber(min, max)
+{
+    return Math.random() * (max - min) + min;
+}
+
+function startFire()
+{
+  var browserWidth = $(window).width();
+  var smoke = $("#smoke");
+  var numberOfSparks = browserWidth < 700 ? 25 : 35;
+  for(i = 0; i < numberOfSparks; i++)
+  {
+    var smokeDelay = randomNumber(1000, 10000);
+    setTimeout(function()
+    {
+      var smokePos = (browserWidth < 700 ? randomNumber(5, 95) : randomNumber(25, 75)) + "%";
+      var newSmoke = smoke.clone();
+      newSmoke.css("left", smokePos);
+      $("body").append(newSmoke);
+    }, smokeDelay);  
+  }
+}
+
+$(function()
+{
+  startFire();
+});
