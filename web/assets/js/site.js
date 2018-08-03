a8450e0545081c285cc839d9d3f076694c412ec1
Jack Sutherland <jacksutherl@gmail.com>
2018-08-03 15:58:41 -0400

    progress

--- web/assets/js/site.js
+++ web/assets/js/site.js
@@ -7,10 +7,10 @@ function startFire()
 {
   var browserWidth = $(window).width();
   var smoke = $("#smoke");
-  var numberOfSparks = browserWidth < 700 ? 50 : 50;
+  var numberOfSparks = browserWidth < 700 ? 30 : 30;
   for(i = 0; i < numberOfSparks; i++)
   {
-    var smokeDelay = randomNumber(0, 30000);
+    var smokeDelay = randomNumber(0, 10000);
     setTimeout(function()
     {
       var smokePos = (browserWidth < 700 ? randomNumber(5, 95) : randomNumber(25, 75)) + "%";
