28a9f09548221035843f8f261e9f7f23975298d4
Jack Sutherland <jacksutherl@gmail.com>
2018-08-06 18:07:29 -0400

    more cool stuff

--- web/assets/js/site.js
+++ web/assets/js/site.js
@@ -1,3 +1,6 @@
+var win;
+var head;
+
 function randomNumber(min, max)
 {
     var rand = Math.random() * (max - min) + min;
@@ -6,7 +9,6 @@ function randomNumber(min, max)
 
 function startFire()
 {
-  var win = $(window);
   var browserWidth = win.width();
   var smoke = $("#smoke");
   var numberOfSparks = browserWidth < 700 ? 15 : 50;
@@ -17,16 +19,52 @@ function startFire()
     {
       //var smokeLeft = (browserWidth < 700 ? randomNumber(5, 95) : randomNumber(25, 75)) + "%";
       var smokeLeft = (browserWidth < 700 ? 50 : randomNumber(25, 75)) + "%";
-      var smokeTop = win.scrollTop() + win.height()
-      var newSmoke = smoke.clone();
-      console.log("smoke pos " + smokeLeft + " " + smokeTop);
-      newSmoke.css({ "left": smokeLeft, "top": smokeTop }).addClass("smoke" + randomNumber(1, 3));
-      $("body").append(newSmoke);
+      var smokeTop = win.scrollTop() + win.height();
+      //console.log(smokeTop);
+      if(smokeTop < 1700)
+      {
+        var newSmoke = smoke.clone();
+        newSmoke.css({ "left": smokeLeft, "top": smokeTop }).addClass("smoke" + randomNumber(1, 3));
+        $("body").append(newSmoke);
+      }
     }, smokeDelay);  
+  }
+}
+
+function checkWinPos(isOnScroll)
+{
+  if(win.scrollTop() === 0)
+  {
+    if(isOnScroll)
+    {
+      head.removeClass("not-at-top").addClass("at-top");
+    }
+  }
+  if(win.scrollTop() > 100)
+  {
+    head.removeClass("at-top").addClass("not-at-top");
   }
 }
 
 $(function()
 {
+  win = $(window);
+  head = $("header");
+
   startFire();
+  checkWinPos();
+
+  win.scroll(function()
+  {
+    checkWinPos(true);
+  });
+
+  $("#main-menu a").click(function(e)
+  {
+    e.preventDefault();
+    var jump = $(this).attr('href');
+    var new_position = jump == "#" ? 0 : ($(jump).offset().top - 100);
+    $('html, body').stop().animate({ scrollTop: new_position }, 500);
+  });
+
 });
