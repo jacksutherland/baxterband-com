35829ded24b4ef77e75843b6f1fab9b4da01e299
Jack Sutherland <jacksutherl@gmail.com>
2020-07-22 23:48:38 -0400

    contact email

--- web/php/mailer.php
+++ web/php/mailer.php
@@ -18,21 +18,21 @@ $msg .= $_POST["message"] . "\n";
 // use wordwrap() if lines are longer than 70 characters
 //$msg = wordwrap($msg,70);
 
-$fromEmail = test_input($_POST["email"]);
-if (filter_var($fromEmail, FILTER_VALIDATE_EMAIL))
-{
-	$fromEmail = 'From: "' . $name . '" <' . $_POST["email"] . '>';
-}
-else
-{
-	$fromEmail = 'From: "Exit 85" <no-reply@exiteightyfive.com>';
-}
+// $fromEmail = test_input($_POST["email"]);
+// if (filter_var($fromEmail, FILTER_VALIDATE_EMAIL))
+// {
+// 	$fromEmail = 'From: "' . $name . '" <' . $_POST["email"] . '>';
+// }
+// else
+// {
+// 	$fromEmail = 'From: "Exit 85" <no-reply@exiteightyfive.com>';
+// }
 
 // send email
-mail("jack@exiteightyfive.com", "Exit 85 Website Contact", $msg, $fromEmail);
-//{
-//	mail("jacksutherl@gmail.com", "Exit 85 Website Failed to Send Contact", $msg, 'From: "Exit 85" <jack@exiteightyfive.com>');
-//}
+if(!mail("jack@exiteightyfive.com", "Exit 85 Website Contact", $msg, 'From: "Exit 85" <no-reply@exiteightyfive.com>'))
+{
+	mail("jacksutherl@gmail.com", "Exit 85 Website Failed to Send Contact", $msg, 'From: "Exit 85" <jack@exiteightyfive.com>');
+}
 //mail("jack@jacksutherland.com", "Band Contact", $msg, 'From: "Exit 85" <booking@exiteightyfive.com>');
 //mail("exit85160@gmail.com", "Band Contact", $msg, 'From: "Exit 85" <booking@exiteightyfive.com>');
 //mail("maxer28@yahoo.com", "Band Contact", $msg, 'From: "Exit 85" <no-reply@exiteightyfive.com>');
